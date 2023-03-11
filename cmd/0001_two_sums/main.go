9116fd54ed51f21f10e9237f88e7b7724cfd860c
Suleymanov Denis <kingmidas1992@gmail.com>
2023-03-11 20:35:00 +0500

    handle first cmd argument

--- cmd/0001_two_sums/main.go
+++ cmd/0001_two_sums/main.go
@@ -12,18 +12,18 @@ import (
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 */
 func main() {
-	if len(os.Args) < 3 {
+	if len(os.Args) < 4 {
 		panic("Not enough arguments")
 	}
 
-	requiredSum, err := strconv.Atoi(os.Args[0])
+	requiredSum, err := strconv.Atoi(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
 
 	nums := make([]int, len(os.Args)-1)
-	for i := 1; i < len(os.Args); i++ {
-		nums[i-1], err = strconv.Atoi(os.Args[i])
+	for i := 2; i < len(os.Args); i++ {
+		nums[i-2], err = strconv.Atoi(os.Args[i])
 		if err != nil {
 			panic(err)
 		}
